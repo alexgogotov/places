1182ba3f1b4736077243ee824ec40abc21ae21a5
alexgogotov <gogotov@gmail.com>
2021-05-26 12:31:30 +0300

    Cделал отдельные виджеты из частей экранов

--- lib/ui/screen/sight_card_want_to_visit.dart
+++ lib/ui/screen/sight_card_want_to_visit.dart
@@ -11,77 +11,99 @@ class SightCardWantToVisit extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Container(
-            width: double.infinity,
-            height: 96,
-            margin: EdgeInsets.symmetric(horizontal: 16),
-            decoration: BoxDecoration(
-                color: Colors.green,
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(25.0),
-                    topRight: Radius.circular(25.0)),
-                image: DecorationImage(
-                  image: NetworkImage(sight.url),
-                  fit: BoxFit.cover,
-                )),
-            child: Stack(
-              children: [
-                Positioned(
-                  top: 19,
-                  right: 18,
-                  child: Icon(
-                    Icons.close,
-                    color: Colors.white,
-                  ),
-                ),
-                Positioned(
-                  top: 19,
-                  right: 56,
-                  child: Icon(
-                    Icons.calendar_today_rounded,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            )),
-        Container(
-            width: double.infinity,
-            margin: EdgeInsets.only(left: 16, right: 16, bottom: 16),
-            decoration: BoxDecoration(
-                color: const Color(0xfff5f5f5),
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(25.0),
-                    bottomRight: Radius.circular(25.0))),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: 16),
-                Container(
-                    margin: EdgeInsets.only(left: 16, right: 16, bottom: 2),
-                    child: Text(
-                      sight.name,
-                      style: text,
-                    )),
-                Container(
-                    height: 28,
-                    margin: EdgeInsets.only(left: 16, right: 16, bottom: 2),
-                    child: Text(
-                      sight.comment,
-                      style: smallGreen,
-                      overflow: TextOverflow.ellipsis,
-                    )),
-                Container(
-                    margin: EdgeInsets.only(left: 16, right: 16, bottom: 16),
-                    child: Text(
-                      sight.hours,
-                      style: smallBlueGrey,
-                      overflow: TextOverflow.ellipsis,
-                    ))
-              ],
-            )),
+        WidgetUp(sight.url),
+        WidgetDown(sight.name, sight.comment, sight.hours),
       ],
     );
+  }
+}
+
+class WidgetUp extends StatelessWidget {
+  final String url;
+
+  WidgetUp(this.url);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        height: 96,
+        margin: EdgeInsets.symmetric(horizontal: 16),
+        decoration: BoxDecoration(
+            color: Colors.green,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25.0),
+                topRight: Radius.circular(25.0)),
+            image: DecorationImage(
+              image: NetworkImage(url),
+              fit: BoxFit.cover,
+            )),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 19,
+              right: 18,
+              child: Icon(
+                Icons.close,
+                color: Colors.white,
+              ),
+            ),
+            Positioned(
+              top: 19,
+              right: 56,
+              child: Icon(
+                Icons.calendar_today_rounded,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class WidgetDown extends StatelessWidget {
+  final String name, comment, hours;
+
+  WidgetDown(this.name, this.comment, this.hours);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: double.infinity,
+        margin: EdgeInsets.only(left: 16, right: 16, bottom: 16),
+        decoration: BoxDecoration(
+            color: const Color(0xfff5f5f5),
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(25.0),
+                bottomRight: Radius.circular(25.0))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(height: 16),
+            Container(
+                margin: EdgeInsets.only(left: 16, right: 16, bottom: 2),
+                child: Text(
+                  name,
+                  style: text,
+                )),
+            Container(
+                height: 28,
+                margin: EdgeInsets.only(left: 16, right: 16, bottom: 2),
+                child: Text(
+                  comment,
+                  style: smallGreen,
+                  overflow: TextOverflow.ellipsis,
+                )),
+            Container(
+                margin: EdgeInsets.only(left: 16, right: 16, bottom: 16),
+                child: Text(
+                  hours,
+                  style: smallBlueGrey,
+                  overflow: TextOverflow.ellipsis,
+                ))
+          ],
+        ));
   }
 }
